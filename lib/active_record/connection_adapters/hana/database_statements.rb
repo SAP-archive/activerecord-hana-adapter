826bcd89793b86c361462b7a9f9a17795834016f
Harsimran Singh Maan <maan.harry@gmail.com>
2014-12-11 11:34:07 -0800

    fIX SYNTAX

--- lib/active_record/connection_adapters/hana/database_statements.rb
+++ lib/active_record/connection_adapters/hana/database_statements.rb
@@ -58,7 +58,7 @@ module ActiveRecord
             stmt.drop if !stmt.nil?
           end
           if has_id and pk
-            val = select_value "SELECT IFNULL(MAX(pk), 0) + 1 FROM #{quote_table_name(table_name)}"
+            val = select_value "SELECT IFNULL(MAX(#{pk}), 0) + 1 FROM #{quote_table_name(table_name)}"
             restart_sequence(default_sequence_name(table_name, nil), val)
           end
         end
