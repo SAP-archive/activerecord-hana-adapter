02f33891b48f9dc8b0a996e448ad3ee44f334223
Harsimran Singh Maan <maan.harry@gmail.com>
2014-12-10 23:42:04 -0800

    Fix syntax

--- lib/active_record/connection_adapters/hana/schema_statements.rb
+++ lib/active_record/connection_adapters/hana/schema_statements.rb
@@ -158,7 +158,6 @@ module ActiveRecord
         end
 
         def add_column(table_name, column_name, type, options = {})
-          if options[:de]
           add_column_sql = "ALTER TABLE #{quote_table_name(table_name)} ADD ( #{quote_column_name(column_name)} #{type_to_sql(type, options[:limit], options[:precision], options[:scale])}"
           add_column_options!(add_column_sql, options)
           add_column_sql << ")"
