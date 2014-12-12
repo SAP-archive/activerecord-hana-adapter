c65f7d3903bf8e1bf6dd029cb71cbc254b5d46b8
Harsimran Singh Maan <maan.harry@gmail.com>
2014-12-12 11:22:14 -0800

    Change order of comparison

--- lib/active_record/connection_adapters/hana/quoting.rb
+++ lib/active_record/connection_adapters/hana/quoting.rb
@@ -19,39 +19,42 @@ module ActiveRecord
           return value.id if value.respond_to?(:quoted_id)
 
           case value
+          when true, false then value ? quoted_true : quoted_false
+
+            # BigDecimals need to be put in a non-normalized form and quoted.
+          when nil        then nil
+          when BigDecimal then value.to_s('F')
+          when Numeric
+            case column.sql_type
+            when "BIGINT" then value.to_s #Fixes RangeError integer XXX too big to convert to `int'
+            else
+              value
+            end
+          when Date, DateTime, Time
+            case column.type
+            when :date then quoted_date(value.to_date)
+            else
+              quoted_date(value)
+            end
           when String, ActiveSupport::Multibyte::Chars
             value = value.to_s
             return value unless column
 
-          case column.type
-          when :binary
-            value = value.unpack("H*")[0]
-            "x'#{value}'"
-          when :integer then value.to_i
-          when :float then value.to_f
-          else
-            value
-          end
-
-          when true, false then value ? quoted_true : quoted_false
-  
-          # BigDecimals need to be put in a non-normalized form and quoted.
-          when nil        then nil
-          when BigDecimal then value.to_s('F')
-          when Numeric 
-            case column.sql_type 
-              when "BIGINT" then value.to_s #Fixes RangeError integer XXX too big to convert to `int'
-              else
-                value
-              end
-          when Date then quoted_date(value.to_date)
-          when DateTime, Time  then quoted_date(value)
+            case column.type
+            when :binary
+              value = value.unpack("H*")[0]
+              "x'#{value}'"
+            when :integer then value.to_i
+            when :float then value.to_f
+            else
+              value
+            end
           when Symbol     then value.to_s
           else
             YAML.dump(value)
           end
         end
-        
+
         def quoted_date(value)
           if value.acts_like?(:time)
             zone_conversion_method = ActiveRecord::Base.default_timezone == :utc ? :getutc : :getlocal
