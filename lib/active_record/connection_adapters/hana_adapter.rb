e43ceb0c3c59c7917f6595a5e82bcce563d688b0
Harsimran Singh Maan <harsimran.maan@sap.com>
2014-12-24 11:46:11 -0800

    Fix syntax

--- lib/active_record/connection_adapters/hana_adapter.rb
+++ lib/active_record/connection_adapters/hana_adapter.rb
@@ -74,6 +74,7 @@ module ActiveRecord
       end
 
       class << self
+
         def value_to_symbol(value)
           case value
           when Symbol
@@ -85,167 +86,167 @@ module ActiveRecord
           end
         end
       end
-    end
 
-    private
-    def extract_enums(enum_string)
-      return nil if enum_string.nil?
-      enum_string.split("','").map { |v| v.intern }
-    end
 
-    def simplified_type(field_type)
-      case field_type
-      when /tinyint/i
-        :boolean
-      else
+      private
+      def extract_enums(enum_string)
+        return nil if enum_string.nil?
+        enum_string.split("','").map { |v| v.intern }
+      end
+
+      def simplified_type(field_type)
+        case field_type
+        when /tinyint/i
+          :boolean
+        else
+          super
+        end
+      end
+
+    end #class HanaColumn
+
+    class HanaAdapter < AbstractAdapter
+
+      include Hana::DatabaseStatements
+      include Hana::SchemaStatements
+      include Hana::Quoting
+
+      def initialize(connection, logger, pool, config)
+        super(connection, logger, pool)
+
+        @schema_cache = Hana::SchemaCache.new self
+        @visitor = Arel::Visitors::Hana.new self
+
+        @connection_options = config
+
+        connect
+        setup_schema
+
+      end
+
+      def connect
+        @connection =
+        if @connection_options[:dsn].include?(';')
+          driver = ODBC::Driver.new.tap do |d|
+            d.name = @connection_options[:dsn_name] || 'Driver1'
+            d.attrs = @connection_options[:dsn].split(';').map{ |atr| atr.split('=') }.reject{ |kv| kv.size != 2 }.inject({}){ |h,kv| k,v = kv ; h[k] = v ; h }
+          end
+          ODBC::Database.new.drvconnect(driver)
+
+        else
+          ODBC.connect @connection_options[:dsn], @connection_options[:username], @connection_options[:password]
+        end.tap do |c|
+          begin
+            c.use_time = true
+            c.use_utc = ActiveRecord::Base.default_timezone == :utc
+          rescue Exception => e
+            warn "Ruby ODBC v0.99992 or higher is required."
+          end
+        end
+      end
+
+      def setup_schema
+        desired_schema = @connection_options[:database]
+        if not schemas.include? desired_schema
+          create_schema desired_schema
+        end
+        set_schema desired_schema
+      end
+
+      # === Abstract Adapter ========================================== #
+
+      # Returns the human-readable name of the adapter.
+      def adapter_name
+        'Hana'
+      end
+
+      def clear_cache!
+        super
+        @statements.clear if !@statements.nil?
+      end
+
+      # Should primary key values be selected from their corresponding
+      # sequence before the insert statement? If true, next_sequence_value
+      # is called before each insert to set the record's primary key.
+      # This is false for all adapters but Firebird.
+      #
+      # This value is set to false, because the adapter auto-generate a
+      # sequence for each table. If this value would be set to true the
+      # user has to spezify a sequence for each table in the migration.
+      def prefetch_primary_key?(table_name = nil)
+        true
+      end
+
+      def requires_reloading?
+        false
+      end
+
+      def supports_bulk_alter?
+        false
+      end
+
+      def supports_count_distinct?
+        true
+      end
+
+      def supports_ddl_transactions?
+        false
+      end
+
+      def supports_explain?
+        true
+      end
+
+      def supports_index_sort_order?
+        false
+      end
+
+      def supports_migrations?
+        true
+      end
+
+      def supports_primary_key?
+        true
+      end
+
+      def supports_savepoints?
+        false
+      end
+
+      def supports_statement_cache?
+        true
+      end
+
+      def primary_key(table_name)
+        row = select_values "SELECT LOWER(COLUMN_NAME) FROM CONSTRAINTS WHERE SCHEMA_NAME=\'#{@connection_options[:database].upcase}\' AND TABLE_NAME=\'#{table_name.upcase}\' AND IS_PRIMARY_KEY=\'TRUE\'"
+        row && row.first
+      end
+
+      # === Abstract Adapter (Connection Management) ================== #
+
+      def active?
+        @connection.do "SELECT 1 FROM DUMMY"
+        true
+      rescue ODBC::Error
+        false
+      end
+
+      def reconnect!
+        disconnect!
+        connect
+        active?
+      end
+
+      def disconnect!
+        @connection.disconnect rescue nil
+      end
+
+      def reset!
+        clear_cache!
         super
       end
     end
 
-  end #class HanaColumn
-
-  class HanaAdapter < AbstractAdapter
-
-    include Hana::DatabaseStatements
-    include Hana::SchemaStatements
-    include Hana::Quoting
-
-    def initialize(connection, logger, pool, config)
-      super(connection, logger, pool)
-
-      @schema_cache = Hana::SchemaCache.new self
-      @visitor = Arel::Visitors::Hana.new self
-
-      @connection_options = config
-
-      connect
-      setup_schema
-
-    end
-
-    def connect
-      @connection =
-      if @connection_options[:dsn].include?(';')
-        driver = ODBC::Driver.new.tap do |d|
-          d.name = @connection_options[:dsn_name] || 'Driver1'
-          d.attrs = @connection_options[:dsn].split(';').map{ |atr| atr.split('=') }.reject{ |kv| kv.size != 2 }.inject({}){ |h,kv| k,v = kv ; h[k] = v ; h }
-        end
-        ODBC::Database.new.drvconnect(driver)
-
-      else
-        ODBC.connect @connection_options[:dsn], @connection_options[:username], @connection_options[:password]
-      end.tap do |c|
-        begin
-          c.use_time = true
-          c.use_utc = ActiveRecord::Base.default_timezone == :utc
-        rescue Exception => e
-          warn "Ruby ODBC v0.99992 or higher is required."
-        end
-      end
-    end
-
-    def setup_schema
-      desired_schema = @connection_options[:database]
-      if not schemas.include? desired_schema
-        create_schema desired_schema
-      end
-      set_schema desired_schema
-    end
-
-    # === Abstract Adapter ========================================== #
-
-    # Returns the human-readable name of the adapter.
-    def adapter_name
-      'Hana'
-    end
-
-    def clear_cache!
-      super
-      @statements.clear if !@statements.nil?
-    end
-
-    # Should primary key values be selected from their corresponding
-    # sequence before the insert statement? If true, next_sequence_value
-    # is called before each insert to set the record's primary key.
-    # This is false for all adapters but Firebird.
-    #
-    # This value is set to false, because the adapter auto-generate a
-    # sequence for each table. If this value would be set to true the
-    # user has to spezify a sequence for each table in the migration.
-    def prefetch_primary_key?(table_name = nil)
-      true
-    end
-
-    def requires_reloading?
-      false
-    end
-
-    def supports_bulk_alter?
-      false
-    end
-
-    def supports_count_distinct?
-      true
-    end
-
-    def supports_ddl_transactions?
-      false
-    end
-
-    def supports_explain?
-      true
-    end
-
-    def supports_index_sort_order?
-      false
-    end
-
-    def supports_migrations?
-      true
-    end
-
-    def supports_primary_key?
-      true
-    end
-
-    def supports_savepoints?
-      false
-    end
-
-    def supports_statement_cache?
-      true
-    end
-
-    def primary_key(table_name)
-      row = select_values "SELECT LOWER(COLUMN_NAME) FROM CONSTRAINTS WHERE SCHEMA_NAME=\'#{@connection_options[:database].upcase}\' AND TABLE_NAME=\'#{table_name.upcase}\' AND IS_PRIMARY_KEY=\'TRUE\'"
-      row && row.first
-    end
-
-    # === Abstract Adapter (Connection Management) ================== #
-
-    def active?
-      @connection.do "SELECT 1 FROM DUMMY"
-      true
-    rescue ODBC::Error
-      false
-    end
-
-    def reconnect!
-      disconnect!
-      connect
-      active?
-    end
-
-    def disconnect!
-      @connection.disconnect rescue nil
-    end
-
-    def reset!
-      clear_cache!
-      super
-    end
-  end
-
-end #module ConnectionAdapters
+  end #module ConnectionAdapters
 
 end #module ActiveRecord
