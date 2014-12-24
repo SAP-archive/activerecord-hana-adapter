72ad8eda8986b5bb6afddbadbae759afe37d496e
Harsimran Singh Maan <maan.harry@gmail.com>
2014-12-24 14:32:13 -0800

    Update hana_adapter.rb

--- lib/active_record/connection_adapters/hana_adapter.rb
+++ lib/active_record/connection_adapters/hana_adapter.rb
@@ -33,6 +33,14 @@ module ActiveRecord
   end
 
   module ConnectionAdapters
+    
+    class TableDefinition
+      def enum(*args)
+        options = args.extract_options!
+        column_names = args
+        column_names.each { |name| column(name, 'enum', options) }
+      end
+    end
 
     class HanaColumn < Column
 
